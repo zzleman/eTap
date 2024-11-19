a52a885897b389e05a4b81729736ef4df51250e6
zzleman <lamanmz@code.edu.az>
2024-11-19 23:56:36 +0400

    chore: update .gitignore to include .env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: 'AIzaSyALuI_HwzrvSA8Lr4ek1nWFBTaX_6uknvc',
-    authDomain: 'e-tap-68eac.firebaseapp.com',
-    projectId: 'e-tap-68eac',
-    storageBucket: 'e-tap-68eac.firebasestorage.app',
-    messagingSenderId: '1024532287155',
-    appId: '1:1024532287155:web:ff0a52d1ceef156fe5aac6',
-    measurementId: 'G-6DCCHR42ZF',
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
